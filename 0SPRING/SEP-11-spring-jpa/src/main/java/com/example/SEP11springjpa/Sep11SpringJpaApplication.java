2a62abcedc3e01183a467efa11ea4fee7f522cfb
neerras13 <neeraja99shwetha@gmail.com>
2019-09-11 17:51:04 +0530

    final jpa commit

---
+++ 0SPRING/SEP-11-spring-jpa/src/main/java/com/example/SEP11springjpa/Sep11SpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SEP11springjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep11SpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sep11SpringJpaApplication.class, args);
+	}
+
+}
