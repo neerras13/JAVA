2d56ba743286d2e735c72bda4e04890be35268ed
neerras13 <neeraja99shwetha@gmail.com>
2019-09-11 17:55:28 +0530

    edit files

--- 0SPRING/SEP-11-spring-jpa/src/main/java/com/example/SEP11springjpa/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SEP11springjpa.repository;
-import com.example.SEP11springjpa.model.Book;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
-
-}
